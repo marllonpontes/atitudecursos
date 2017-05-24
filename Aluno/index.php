c1bcdbb3f83929c8dc3406e27896dd66371b7691
Atitude Cursos <atituderhcursos@gmail.com>
2017-05-24 17:00:15 -0300

    Update index.php

--- Aluno/index.php
+++ Aluno/index.php
@@ -7,7 +7,16 @@ protegePagina();
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-<?php include_once("../php/analyticstracking.php"); ?>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-88350404-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
       <meta charset="utf-8" />
       <link rel="shortcut icon" href="../images/ico/favicon.png">
     <link rel="apple-touch-icon-precomposed" sizes="144x144" href="../images/ico/apple-touch-icon-144-precomposed.png">
